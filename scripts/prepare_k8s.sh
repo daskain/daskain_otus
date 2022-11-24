810077671d54220cc5ed9b036671963ff632e444
daskain <daskain@gmail.com>
2022-11-24 11:30:48 +0300

    step-4 finish

--- scripts/prepare_k8s.sh
+++ scripts/prepare_k8s.sh
@@ -1,25 +1,12 @@
-# #! /bin/bash
+#! /bin/bash
 
-#VAR
+
 PROJECT_FOLDER="/home/daskain/git/dev/otus/daskain_otus"
-
-#get cred
-# yc managed-kubernetes cluster get-credentials \
-#     --folder-name $K8S_FOLDER_NAME \
-#     --cloud-id $K8S_CLOUD_ID \
-#     --external $K8S_MASTER_NODE \
-#     --force
 
 cd $(echo $PROJECT_FOLDER/kuber/manifests)
 
 echo "Install Ingress Contorller"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.0/deploy/static/provider/cloud/deploy.yaml
-
-# echo "Create admin account"
-# kubectl apply -f gitlab-admin-service-account.yaml
-
-# echo "Registration runner role"
-# kubectl apply -f gitlab-runner-role.yaml
 
 echo "Create admin account"
 kubectl create clusterrolebinding gitlab-admin --clusterrole=cluster-admin --group=system:serviceaccounts
@@ -29,8 +16,6 @@ helm upgrade --namespace default gitlab-runner -f values.yaml gitlab/gitlab-runn
 
 echo "Create monitoring"
 kubectl create namespace monitoring
-
-echo "Add serviceMonitor"
 
 #GET token
 token=$(kubectl -n kube-system get secrets -o json | \
